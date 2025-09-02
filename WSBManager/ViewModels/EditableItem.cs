9c088690f136329106903d1039dee85128b6b6e5
arendellian13 <listener1381@outlook.com>
2025-09-02 11:27:00 +0800

    Add EditableItem class and implement tab management in MainWindowViewModel

---
+++ WSBManager/ViewModels/EditableItem.cs
@@ -0,0 +1,15 @@
+﻿using ReactiveUI;
+using ReactiveUI.Fody.Helpers;
+
+namespace WSBManager.ViewModels;
+
+public class EditableItem<T>(T value) : ReactiveObject
+{
+    private T _value = value;
+
+    public T Value
+    {
+        get => _value;
+        set => this.RaiseAndSetIfChanged(ref _value, value);
+    }
+}
--- WSBManager/ViewModels/MainWindowViewModel.cs
+++ WSBManager/ViewModels/MainWindowViewModel.cs
@@ -1,20 +1,50 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Diagnostics;
 using System.Linq;
+using System.Reactive;
+using System.Reactive.Disposables;
+using System.Reactive.Linq;
 using System.Xml;
+using DynamicData;
+using DynamicData.Binding;
 using ReactiveUI;
+using ReactiveUI.Fody.Helpers;
 using WSBManager.Models;
 
 namespace WSBManager.ViewModels;
 
 public class MainWindowViewModel : ReactiveObject
 {
+    private readonly SourceList<EditableItem<string>> _tabsSource = new();
+    private readonly ReadOnlyObservableCollection<EditableItem<string>> _tabs;
+    public ReadOnlyObservableCollection<EditableItem<string>> Tabs => _tabs;
+
+    public ReactiveCommand<Unit, Unit> AddTab { get; }
+    [Reactive] public int SelectedTabIndex { get; set; } = -1;
+
     public SandboxInstanceViewModel SandboxInstanceViewModel { get; }
 
     public MainWindowViewModel(SandboxInstanceViewModel sandboxInstanceViewModel)
     {
         SandboxInstanceViewModel = sandboxInstanceViewModel;
+
+        _tabsSource.Connect()
+            .Bind(out _tabs)
+            .Subscribe();
+
+        AddTab = ReactiveCommand.Create(AddNewTab);
+
         Debug.WriteLine("MainWindowViewModel initialized");
+    }
+
+    private void AddNewTab()
+    {
+        var newTabName = $"Tab {_tabs.Count + 1}";
+        _tabsSource.Add(new EditableItem<string>(newTabName));
+        Debug.WriteLine($"Current tabs: {string.Join(", ", _tabs.Select(t => t.Value))}");
+        Debug.WriteLine($"Selected tab index before adding: {SelectedTabIndex}");
+        Debug.WriteLine($"Added new tab: {newTabName}");
     }
 }
